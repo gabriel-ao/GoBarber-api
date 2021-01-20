f9eccdefd18f1719869b64a3d04f98954560092e
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2021-01-20 16:24:43 -0300

    modulo 04 - aula 23 - configurando variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '56c7bf74158b88',
-    pass: 'f137463a7a64ca',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-  dsn:
-    'https://127a0275b3084032ba6f6bb6f1117563@o508300.ingest.sentry.io/5600601',
+  dsn: process.env.SENTRY_DSN,
 };
