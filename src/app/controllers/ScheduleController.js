b93789a178f0e0b3228ef95b2046e425224051aa
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2021-02-08 18:18:47 -0300

    Padronizando para a aplicação web

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -26,6 +26,14 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+      order: ['date'],
     });
 
     return res.json(appointments);
